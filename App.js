a2bb613518af4951a6dac9788195fbceab686dd4
zahidalidev <m.zahidalidev@gmail.com>
2021-06-18 08:42:22 +0500

    font

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { LogBox, Dimensions } from "react-native";
+import { LogBox, ActivityIndicator, View } from "react-native";
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Karla_400Regular, Karla_700Bold, useFonts } from "@expo-google-fonts/karla"
 
 // components
 import HomeTab from './app/components/HomeTabs';
 
+import { RFPercentage } from 'react-native-responsive-fontsize';
+import Colors from './app/config/Colors';
+
 const Stack = createStackNavigator()
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold
+  });
+
+
+  if (!fontsLoaded) return <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }} >
+    <ActivityIndicator size={RFPercentage(6)} color={Colors.primary} />
+  </View>
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="HomeTabs" >
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ImageBackground, Dimensions, Text, Image } from 'react-native';
+import { Button } from 'react-native-paper';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+
+import img from "../assets/images/Rectangle2.png"
+import LocaIcon from "../assets/icons/locationTick.png"
+
+import Colors from '../config/Colors';
+
+const height = Dimensions.get('window').height
+const width = Dimensions.get('window').width
 
 function HomeScreen(props) {
     return (
-        <View style={styles.container}></View>
+        <ImageBackground resizeMode="stretch" source={img} style={styles.backgroundImage}>
+            <Button style={{ flexDirection: "row", width: RFPercentage(34), backgroundColor: Colors.primary, height: RFPercentage(7.25), justifyContent: "center", alignItems: "center" }} >
+                <Image source={LocaIcon} style={{ marginRight: 15 }} />
+                <Text style={{ fontFamily: "Karla_400Regular", marginLeft: 10, fontSize: RFPercentage(2.2), color: Colors.white }} >Trouver mon adresse</Text>
+            </Button>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-
+    backgroundImage: {
+        height,
+        width,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        justifyContent: "center",
+        alignItems: "center"
     }
 })
 
