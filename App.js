02ce94730f6a36e6915e8bd6cfcbc68f72e848ff
zahidalidev <m.zahidalidev@gmail.com>
2021-06-18 07:26:37 +0500

    bottom tabs

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { LogBox, Dimensions } from "react-native";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather, Ionicons, MaterialIcons } from "@expo/vector-icons"
+import { RFPercentage } from "react-native-responsive-fontsize";
+
+// Colors
+import HomeScreen from './app/screens/HomeScreen';
+
+// config
+import Colors from './app/config/Colors';
+
+const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
 
 export default function App() {
+
+
+  const HomeTab = ({ navigation }) => (
+    <Tab.Navigator initialRouteName="Home" tabBarOptions={{
+      style: { height: RFPercentage(7.5) },
+      labelStyle: { fontSize: 0 },
+      tabStyle: { backgroundColor: Colors.primary },
+      activeTintColor: Colors.secondary, inactiveTintColor: Colors.white
+    }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather
+              name="home"
+              color={color}
+              size={RFPercentage(3.3)}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              name="alternate-email"
+              color={color}
+              size={RFPercentage(3.3)}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CreateRecipe"
+        component={HomeScreen}
+        options={{
+
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name="person-circle-outline"
+              color={color}
+              size={RFPercentage(3.3)}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="HomeTabs" >
+        <Stack.Screen name="HomeTabs" component={HomeTab} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ app/config/Colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    primary: "#0000A1",
+    secondary: "#39BAE8",
+    white: "#FFFFFF",
+    black: "#000000",
+    grey: ""
+}
+
+export default Colors;
